65c2db04e35e8e6e50803b864658347357166db6
Lee0326 <lcyfly1@163.com>
2019-07-18 20:55:55 +0800

    chore:Fix conflicts

--- line_segment/cmake_install.cmake
+++ line_segment/cmake_install.cmake
@@ -1,4 +1,4 @@
-# Install script for directory: /home/colin/catkin_ls_ws/src/line_segment
+# Install script for directory: /home/lee/Sineva/LS_ELAS/line_segment
 
 # Set the install prefix
 if(NOT DEFINED CMAKE_INSTALL_PREFIX)
@@ -32,11 +32,6 @@ if(NOT DEFINED CMAKE_INSTALL_SO_NO_EXE)
   set(CMAKE_INSTALL_SO_NO_EXE "1")
 endif()
 
-# Is this installation the result of a crosscompile?
-if(NOT DEFINED CMAKE_CROSSCOMPILING)
-  set(CMAKE_CROSSCOMPILING "FALSE")
-endif()
-
 if(CMAKE_INSTALL_COMPONENT)
   set(CMAKE_INSTALL_MANIFEST "install_manifest_${CMAKE_INSTALL_COMPONENT}.txt")
 else()
@@ -45,5 +40,5 @@ endif()
 
 string(REPLACE ";" "\n" CMAKE_INSTALL_MANIFEST_CONTENT
        "${CMAKE_INSTALL_MANIFEST_FILES}")
-file(WRITE "/home/colin/catkin_ls_ws/src/line_segment/${CMAKE_INSTALL_MANIFEST}"
+file(WRITE "/home/lee/Sineva/LS_ELAS/line_segment/${CMAKE_INSTALL_MANIFEST}"
      "${CMAKE_INSTALL_MANIFEST_CONTENT}")
